2f1e1289bee60ab928ec2a33df649325c0aaefa8
Danila Aleksin <danya09kz@gmail.com>
2023-08-27 17:46:44 +0600

    fix

--- src/components/AboutPage/AboutPromo/AboutPromo.tsx
+++ src/components/AboutPage/AboutPromo/AboutPromo.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 const AboutPromo = () => {
 	return (
 		<div className="relative bg-white flex items-center dark:bg-black min-h-[50vh] md:min-h-[60vh] overflow-hidden">
-			<div className="absolute z-0 top-0 left-0 right-0 bottom-0 overflow-hidden blur-[100px] select-none pointer-events-none" role="region">
+			<div className="absolute z-0 top-0 left-0 right-0 bottom-0 overflow-hidden blur-[50px] sm:blur-[100px] select-none pointer-events-none" role="region">
 				<div className="grad grad-1"></div>
 				<div className="grad grad-2"></div>
 				<div className="grad grad-3"></div>
