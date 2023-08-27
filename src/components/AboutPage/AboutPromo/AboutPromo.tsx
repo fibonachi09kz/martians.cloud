bf586a67f7cc6b700b5ccef23d40ec72eb8c2f17
Danila Aleksin <danya09kz@gmail.com>
2023-08-27 17:48:17 +0600

    fix

--- src/components/AboutPage/AboutPromo/AboutPromo.tsx
+++ src/components/AboutPage/AboutPromo/AboutPromo.tsx
@@ -16,14 +16,14 @@ const AboutPromo = () => {
 					<p className="mx-auto mt-6 max-w-md text-lg text-gray-800 dark:text-white sm:text-lg md:mt-8 md:max-w-3xl md:text-2xl">
 						Мы - команда экспертов в веб-разработке, с фокусом на создание потрясающих и инновационных веб-решений
 					</p>
-					<div className="mx-auto mt-8 flex-col sm:flex-row flex justify-center gap-5 md:mt-12">
-						<button className="button button-white !text-black !text-lg">
+					<div className="mx-auto mt-8 flex-col sm:flex-row flex justify-center gap-3 sm:gap-5 md:mt-12">
+						<button className="button button-white !text-black sm:!text-lg">
 							<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
 								<path strokeLinecap="round" strokeLinejoin="round" d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9 5.25h.008v.008H12v-.008z" />
 							</svg>
 							Узнать больше
 						</button>
-						<button className="button button-blue !text-white !text-lg">Связаться с нами</button>
+						<button className="button button-blue !text-white sm:!text-lg">Связаться с нами</button>
 					</div>
 				</div>
 			</div>
